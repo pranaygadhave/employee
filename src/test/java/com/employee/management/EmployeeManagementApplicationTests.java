a4d3f04e3332a13efde2deeaedcd5ace5567925a
Pranay Gadhave <gadhavepranay@gmail.com>
2023-04-29 23:13:04 +0530

    docker

---
+++ src/test/java/com/employee/management/EmployeeManagementApplicationTests.java
@@ -0,0 +1,12 @@
+package com.employee.management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeManagementApplicationTests {
+
+	/*
+	 * @Test void contextLoads() { }
+	 */
+}
